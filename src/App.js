69c8b610d347ca2991f5955fee4aae47795eb7aa
Tran <tientra@a483e788dc46.ant.amazon.com>
2021-03-30 13:10:56 -0400

    changing dns to lb1

--- src/App.js
+++ src/App.js
@@ -28,9 +28,7 @@ class App extends Component {
     handleSubmit = (e) => {
       e.preventDefault()
       this.sendingForm(this.state)
-      fetch('http://ec2-3-92-47-162.compute-1.amazonaws.com:3001
-
-',{
+      fetch('http://ec2-3-92-47-162.compute-1.amazonaws.com:3001',{
         method: "GET",
         headers: {"Content-Type": "application/json"}
       })
@@ -40,9 +38,7 @@ class App extends Component {
 
     sendingForm = (data) => {
       console.log(data.name)
-      fetch('http://ec2-3-92-47-162.compute-1.amazonaws.com:3001
-
-', {
+      fetch('http://ec2-3-92-47-162.compute-1.amazonaws.com:3001', {
         method: "POST",
         headers: {
           "Content-Type": 'application/json'
