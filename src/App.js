0a2619cb3c0c1840ecc7821ad6e43300970d6562
Tran <tientra@a483e788dc46.ant.amazon.com>
2021-04-04 02:51:41 -0400

    change DNS1

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     }
 
     componentDidMount = () => {
-      fetch('http://ec2-3-92-47-162.compute-1.amazonaws.com:3001',{
+      fetch('http://10.0.0.224:3001',{
         method: "GET",
         headers: {"Content-Type": "application/json"}
       })
